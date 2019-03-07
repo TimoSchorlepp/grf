2f34bb5474fdf97f1bc11fd2239daff70033fcbd
timo <timo.schorlepp@rub.de>
2019-03-07 16:09:04 +0100

    Add correlation computations to kpz script and kpz  heun scheme to slides

--- src/kpz1d.py
+++ src/kpz1d.py
@@ -9,27 +9,26 @@ class kpz1d(object):
 	# h could be the height of a surface upon which particles are deposited
 	# xi is centered Gaussian, delta-correlated in time and spatially correlated on a scale l, with intensity D
 	# boundaries are periodic
-	
-    def __init__(self, nu, lbda, D, l, N, L):
-        self.nu = nu # diffusion strength
-        self.lbda = lbda # deposition rate
-        self.D = D # intensity of noise / particle arrival on surface
-        self.N = N # number of gridpoints in space
-        self.l = l # correlation length of xi in space
-        self.L = L # interval length [0,L) which is discretized
-        self.dx = L/N
-        self.dt = 0.25 * self.dx**2/self.D # CFL (deterministic)
-        self.steps = 0 # count number of steps..
-        self.xi = rdf1.RandField1d(l,1.,L,N) # intensity of xi is D/dx
-        
-        #initial condition for surface height
-        self.h = np.zeros(N)
-        #~ self.h[0] = 5.
-        X = np.linspace(0,(N-1)*self.dx,N)
-        for i in range(N):
+	def __init__(self, nu, lbda, D, l, N, L):
+		self.nu = nu # diffusion strength
+		self.lbda = lbda # deposition rate
+		self.D = D # intensity of noise / particle arrival on surface
+		self.N = N # number of gridpoints in space
+		self.l = l # correlation length of xi in space
+		self.L = L # interval length [0,L) which is discretized
+		self.dx = L/N
+		self.dt = 0.25 * self.dx**2/self.nu # CFL (deterministic)
+		self.steps = 0 # count number of steps..
+		self.time = 0 # count elapsed time
+		self.xi = rdf1.RandField1d(l,1.,L,N) # intensity of xi is D/dx
+		#initial condition for surface height
+		self.h = np.zeros(N)
+		#~ self.h[0] = 5.
+		X = np.linspace(0,(N-1)*self.dx,N)
+		for i in range(N):
 			self.h[i] = np.min([abs(X[0]-X[i]),abs(X[0]-X[i]+L),abs(X[0]-X[i]-L)])
-    def step(self):
-		
+	
+	def step(self):
 		h = self.h
 		Xi = self.xi.getFieldRealizationRealSpaceSpectral()
 		#~ Xi = np.random.randn(self.N)/np.sqrt(self.dx)
@@ -58,19 +57,31 @@ class kpz1d(object):
 			else:
 				break
 		self.dt = dt
+		self.time += dt
 		self.h = hnew
 		self.steps += 1
 	
-    def multistep(self, n):
-        for i in range(n):
-            self.step()
+	def multistep(self, n):
+		for i in range(n):
+			self.step()
+	
+	def stepUntilT(self,T):
+		while self.time < T:
+			self.step()
+	
+	def getdhdt(self,n):
+		h_old = self.h
+		time_old = self.time
+		self.multistep(n)
+		return (np.sum(self.h)-np.sum(h_old))/(self.time-time_old)
+		
 
 def init():
     line.set_data([], [])
     return line,
 
 def animate(i):
-    simulation.multistep(1)
+    simulation.multistep(100)
     h = simulation.h
     hmean = sum(h)/simulation.N
     hmean = 0
@@ -82,25 +93,73 @@ def animate(i):
 nu = 1.
 lbda = 1.
 D = 1.
-l = 0.1
+l = 0.0000001
 N = 128
 L = 2 * np.pi
 dx = L/N
 
-simulation = kpz1d(nu,lbda,D,l,N,L)
+###################################################
+# uncomment for animation of surface growth 
+#~ simulation = kpz1d(nu,lbda,D,l,N,L)
+#~ x_values = np.linspace(0, (N - 1) * dx, N)
+#~ plot_xmax = N * dx
+#~ plot_hmin, plot_hmax = -1.,5.
+#~ plot_height = plot_hmax - plot_hmin
+#~ fig = plt.figure()
+#~ plt.axes(xlim=(0, plot_xmax), ylim=(plot_hmin, plot_hmax))
 
-x_values = np.linspace(0, (N - 1) * dx, N)
-plot_xmax = N * dx
-plot_hmin, plot_hmax = -1.,5.
-plot_height = plot_hmax - plot_hmin
-fig = plt.figure()
-plt.axes(xlim=(0, plot_xmax), ylim=(plot_hmin, plot_hmax))
-
-line = plt.plot([], [])[0]
+#~ line = plt.plot([], [])[0]
             
-plt.xlabel('$x$', fontsize=16)
-plt.ylabel(r'$h$', fontsize=16)
+#~ plt.xlabel('$x$', fontsize=16)
+#~ plt.ylabel(r'$h$', fontsize=16)
 
-animationObject = anim.FuncAnimation(fig, animate, init_func=init, blit=True)
+#~ animationObject = anim.FuncAnimation(fig, animate, init_func=init, blit=True)
+#~ plt.show()
 
-plt.show()
+###################################################
+# determine mean time derivative of h
+#~ N_iter = 300 #number of times the simulation os repeated
+#~ dhdtArr = np.zeros(N_iter) # will hold the average time derivative of h per simulation
+#~ for i in range(N_iter):
+	#~ print i
+	#~ simulation = kpz1d(nu,lbda,D,l,N,L)
+	#~ N_multisteps = 100 # number of multisteps per simulation
+	#~ N_onemult = 50 # number of steps per multistep
+	#~ dhdt_one_iter = np.zeros(N_multisteps)
+	#~ for j in range(N_multisteps):
+		#~ dhdt_one_iter[j] = simulation.getdhdt(N_onemult)
+	#~ dhdtArr[i] = np.sum(dhdt_one_iter)/N_multisteps
+
+#~ print np.mean(dhdtArr)
+#~ print np.std(dhdtArr)
+
+# no correlation: 337.73779737587535 +-29.992229916804153
+
+###################################################
+# simulate to a given time in order to determine two-point correlation
+N_iter = 1000
+T = 1.
+hT = np.zeros(N_iter)
+for i in range(N_iter):
+	print i
+	simulation = kpz1d(nu,lbda,D,l,N,L)
+	simulation.stepUntilT(T)
+	hT[i] = simulation.h[0]
+
+simulation = kpz1d(nu,lbda,D,l,N,L)
+C = np.sum((hT - simulation.h[0] - T * 340)**2)/N_iter
+
+# no correlation
+# t = 0.1: C = 
+# t = 0.25: C = 
+# t = 0.5: C =
+# t = 1: C =
+# t = 2: C = 
+# t = 3: C =
+# t = 4: C =
+# t = 5: C = 
+
+
+
+
+		
